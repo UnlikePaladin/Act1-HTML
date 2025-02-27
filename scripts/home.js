97c19d7641d25f4f45cb9f498eb082869310d6a2
UnlikePaladin <36827970+UnlikePaladin@users.noreply.github.com>
2025-02-27 16:49:04 -0600

    mejoras

--- scripts/home.js
+++ scripts/home.js
@@ -1,7 +1,11 @@
 window.onload = () => {
     if (!sessionStorage.name) {
         window.location = "../index.html";
-    } 
+    } else {
+        const h1 = document.getElementById("h1");
+        h1.innerHTML = "Hola " + sessionStorage.name;
+        console.log("hola", sessionStorage.name);
+    }
 }
 
 const logout = document.getElementById("logout");
@@ -9,4 +13,23 @@ const logout = document.getElementById("logout");
 logout.addEventListener("click", () => {
     sessionStorage.removeItem("name")
     window.location = "../index.html";
+});
+
+
+const chat = document.getElementById("chat");
+const msg = document.getElementById("msg");
+const send = document.getElementById("sendBtn");
+
+function sendMsg(){
+        chat.value += "\n" + msg.value;
+        msg.value ="";
+    
+}
+
+send.addEventListener("click", sendMsg);
+
+msg.addEventListener("keydown", (e) => {
+    if (e.key == "Enter") {
+        sendMsg();
+    }
 });
